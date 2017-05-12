022ee186565a16ad77256f6b58fad8d3c00606c7
Beth Evans <baevans_87@hotmail.com>
2017-05-12 18:25:42 -0500

    Solved Project

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,47 @@
 $(function() {
+$.ajax({
+  url: 'https://www.codeschool.com/users/baevans1.json',
+  dataType: 'jsonp',
+  success: function(response) {
+    // handle response
+    console.log('response', response.courses.completed)
+    addCourses(response.courses.completed);
+   }
 
+});
+
+function addCourses(courses) {
+
+
+
+  courses.forEach(function(course){
+    console.log(course);
+    var myCourses = $('<div />', {
+      'class': 'course'
+    }).appendTo('#badges');
+
+    var classTitles = $('<h3 />', {
+      text: course.title
+    }).appendTo(myCourses);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo(myCourses);
+   $('<a />', {
+     'class': 'btn btn-primary',
+     href: course.url,
+     target: '_blank',
+     text: 'See Course'
+
+   }).appendTo(myCourses)
+
+  })
+
+}
+
+
+
+  // for (i = 0, i < response.length)
   // your code will go here
 
 });
